0c1458504fd994886e7cc0500a1872a78d9c7740
Jasvinder Singh <jvsinghk@gmail.com>
2021-05-28 16:40:21 +0530

    Connect redux to the top-level react component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 import { App } from './app/App.js';
 // Import the store here.
-
+import { store } from './app/store'
 // Pass state and dispatch props to the <App /> component.
 const render = () => {
   ReactDOM.render(
     <App 
-      
+      state={store.getState()}
+      dispatch={store.dispatch}
     />,
     document.getElementById('root')
   )
@@ -16,3 +17,4 @@ const render = () => {
 render();
 
 // Subscribe render to the store.
+store.subscribe(render);
